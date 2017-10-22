1b5d793bacde31c262ff456b704def30e0c87023
puolimatkankrouvi <cde_1414@yahoo.com>
2017-10-22 14:33:01 +0300

    Initial commit

---
+++ Veditor/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsForms
+{
+    partial class Veditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textPanel = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(982, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.saveToolStripMenuItem,
+            this.quitMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.openMenuItem.Text = "&Open";
+            this.openMenuItem.Click += new System.EventHandler(this.OpenMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.saveToolStripMenuItem.Text = "&Save As";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // quitMenuItem
+            // 
+            this.quitMenuItem.Name = "quitMenuItem";
+            this.quitMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.quitMenuItem.Size = new System.Drawing.Size(185, 26);
+            this.quitMenuItem.Text = "&Quit";
+            this.quitMenuItem.Click += new System.EventHandler(this.QuitMenuItem_Click);
+            // 
+            // textPanel
+            // 
+            this.textPanel.AcceptsReturn = true;
+            this.textPanel.AcceptsTab = true;
+            this.textPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textPanel.Location = new System.Drawing.Point(0, 28);
+            this.textPanel.MinimumSize = new System.Drawing.Size(200, 200);
+            this.textPanel.Multiline = true;
+            this.textPanel.Name = "textPanel";
+            this.textPanel.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textPanel.Size = new System.Drawing.Size(982, 615);
+            this.textPanel.TabIndex = 1;
+            this.textPanel.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textPanel_KeyPressed);
+            // 
+            // Veditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 643);
+            this.Controls.Add(this.textPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Veditor";
+            this.Text = "Veditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitMenuItem;
+        private System.Windows.Forms.TextBox textPanel;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ Veditor/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace WindowsForms
+{
+    public partial class Veditor : Form
+    {
+
+        public Veditor()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void OpenMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream virta = null;
+            OpenFileDialog avausdialogi = new OpenFileDialog();
+
+            avausdialogi.InitialDirectory = "c://";
+            avausdialogi.Filter = "txt files (*.txt)|*.txt";
+            avausdialogi.FilterIndex = 2;
+            avausdialogi.RestoreDirectory = true;
+
+            //If dialog box opens
+            if (avausdialogi.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((virta = avausdialogi.OpenFile()) != null)
+                    {
+                        using (virta)
+                        {
+
+                            byte[] result = new byte[virta.Length];
+                            await virta.ReadAsync(result, 0, (int)virta.Length);
+
+                            textPanel.Text = Encoding.UTF8.GetString(result);
+
+                            textPanel.Refresh();
+
+                            virta.Close();
+                        }
+                    }
+
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message);
+                }
+            }
+
+        }
+
+        private void QuitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            string sulkemisvaroitus = "Haluatko varmasti lopettaa?";
+            MessageBoxButtons button = MessageBoxButtons.YesNo;
+
+            
+
+
+            switch (e.CloseReason){
+                case (CloseReason.WindowsShutDown):
+                    Application.Exit();
+                    break;
+                default:
+                    DialogResult result = MessageBox.Show(sulkemisvaroitus, "", button);
+
+                    switch (result)
+                    {
+                        case DialogResult.Yes:
+                            break;
+                        default:
+                            e.Cancel = true;
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileStream virta = null;
+            SaveFileDialog saveDialog = new SaveFileDialog();
+
+            saveDialog.InitialDirectory = "c://";
+            saveDialog.Filter = "txt files (*.txt)|*.txt";
+            saveDialog.RestoreDirectory = true;
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((virta = (FileStream)saveDialog.OpenFile()) != null)
+                {
+                    byte[] fileContents = Encoding.UTF8.GetBytes(textPanel.Text);
+
+                    virta.Write(fileContents, 0, fileContents.Length);
+                    virta.Close();
+                }
+            }
+        }
+
+        private void textPanel_KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            string currentText = textPanel.Text;
+
+
+            
+            if (currentText.Length >= 0)
+            {
+                //Sulkee automaattisesti hakasulut '{}'
+                if (e.KeyChar == '{')
+                {
+                    int kursori = textPanel.SelectionStart;
+                    textPanel.Text = textPanel.Text.Insert(textPanel.SelectionStart, "}");
+                    textPanel.SelectionStart = kursori;
+                }
+
+                //Sulkee automaattisesti sulut '()'
+                if (e.KeyChar == '(')
+                {
+                    int kursori = textPanel.SelectionStart;
+                    textPanel.Text = textPanel.Text.Insert(textPanel.SelectionStart, ")");
+                    textPanel.SelectionStart = kursori;
+                }
+
+                //Sulkee ""
+                if (e.KeyChar == '"')
+                {
+                    int kursori = textPanel.SelectionStart;
+                    textPanel.Text = textPanel.Text.Insert(textPanel.SelectionStart, "\"");
+                    textPanel.SelectionStart = kursori;
+                }
+            }
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
